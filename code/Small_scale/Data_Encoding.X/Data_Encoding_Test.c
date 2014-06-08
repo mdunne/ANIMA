d54970238dd72e007260b8304c35ac12e7d8d173
Max Dunne <mdunne@soe.ucsc.edu>
2014-06-08 10:05:49 -0700

    added data encoding along with initial test code for generating crc with DMA

---
+++ code/Small_scale/Data_Encoding.X/Data_Encoding_Test.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+
+#include "BOARD.h"
+#include "timers.h"
+#include "serial.h"
+#include <peripheral/dma.h>
+
+#pragma config FNOSC = FRCPLL
+#pragma config FPLLIDIV = DIV_1
+#pragma config FPLLMUL = MUL_20
+#pragma config FPLLODIV = DIV_2
+#pragma config FPBDIV = DIV_1
+#pragma config FWDTEN = OFF
+#pragma config FSOSCEN = OFF
+#pragma config OSCIOFNC = OFF
+
+#define CRC_PROOF_OF_CONCEPT
+
+void main(void) {
+    BOARD_Init();
+
+
+    printf("Welcome to the Data Encoding Test\r\n");
+#ifdef CRC_PROOF_OF_CONCEPT
+
+    TIMERS_Init();
+    unsigned int iterator;
+    unsigned int starttime,duration;
+    unsigned int DataSize = 512;
+    unsigned char Data[DataSize];
+    unsigned int crc1 = 0, crc2;
+    int DMAChannel = 2;
+    DmaTxferRes res;
+
+    for (iterator = 0; iterator < DataSize; iterator++) {
+        Data[iterator] = iterator;
+    }
+    Data[511]=46;
+    //    printf("Hi Bob\r\n");
+    //    while (1);
+    DmaChnConfigure(DMAChannel, DMA_CHN_PRI2, DMA_OPEN_DEFAULT);
+    DmaSfmConfigure(DMA_CHKSUM_CRC, DMA_BITO_MSb, DMA_REORDER_NOT);
+    DmaSfmCrcConfigure(0x8005, 12, 0xffff);
+    starttime=GetTime();
+    res = DmaChnMemCrc(&crc1, Data, DataSize-1, DMAChannel, DMA_CHN_PRI3);
+    duration=GetTime()-starttime;
+    printf("DMA Transfer Results: %d\r\n", res);
+    printf("CRC calculated: %X\r\n",crc1);
+    //printf("Time taken: %d\r\n",duration);
+    //DmaChnSetTxfer(DMAChannel,Data,&crc1,DataSize);
+    //DmaChnEnable(DMAChannel);
+
+#endif
+    while (1);
+
+    //------------------------------------------------------------------
+
+}
